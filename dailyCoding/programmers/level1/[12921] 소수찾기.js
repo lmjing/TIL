cd1916d7c91a524bfa07401ab2f06bad8db0d6bd
lmjing <lmjing4905@gmail.com>
2021-07-04 12:12:09 +0900

    dailyCoding - 소수찾기(에라토스테네스의 체)

---
+++ dailyCoding/programmers/level1/[12921] 소수찾기.js
@@ -0,0 +1,19 @@
+/**
+ * level 1
+ * 연습 문제
+ * 에라토스테네스의 체(https://mizzo-dev.tistory.com/entry/baekjoon1929)
+ */
+function solution(n) {
+    let primeCheck = Array(n + 1); // 1이면 소수 아님. 값 없어야 소수
+    let count = 0;
+    for(let i=2; i<=n; i++) {
+        if(!primeCheck[i]) { // 소수임
+            count++;
+            for(let j=i*i; j<=n; j+=i) {
+                primeCheck[j] = 1; // 배수들을 지움
+            }
+        }
+    }
+
+    return count;
+}
