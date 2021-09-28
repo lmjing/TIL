93627b5de7b5f8a4c70aaf060faa295c6a047ff9
lmjing <lmjing4905@gmail.com>
2021-09-28 21:05:33 +0900

    dailyCoding - 프로그래머스 week8.js (86491)

---
+++ dailyCoding/programmers/weeklyChallenge/week8.js
@@ -0,0 +1,11 @@
+function solution(sizes) {
+    return sizes.reduce(([mh, mv], [a, b]) => [Math.max(mh, Math.min(a, b)), Math.max(mv, Math.max(a, b))], [0, 0]).reduce((_, $) => _ * $)
+}
+function solution(sizes) {
+    const swap = (_) => [_[1], _[0]];
+    return sizes.reduce((maxs, cur) => {
+        if (cur[0] === Math.min(...cur)) cur = swap(cur);
+        return maxs.map((v, i) => Math.max(v, cur[i]));
+    }, [0, 0])
+        .reduce((_, $) => _ * $)
+}
