79bb49b6687fb5d38fd833b9199f3b53fc3b8efd
lmjing <lmjing4905@gmail.com>
2021-10-07 19:14:41 +0900

    week3 - 풀이 중

--- dailyCoding/programmers/weeklyChallenge/week3.js
+++ dailyCoding/programmers/weeklyChallenge/week3.js
@@ -43,7 +43,7 @@ function solution(game_board, table) {
 
 
     // 2. 게임 보드 순회
-    let answer = 0;
+    let blanks = [];
     game_board.forEach((row, y) => {
         row.forEach((v, x) => {
             if (v === 0) { // 블록 저장
@@ -69,33 +69,55 @@ function solution(game_board, table) {
                     blank.history.push(cellHistory);
                 }
                 blank.area = blank.history.length;
-                // 여기까지 코드 동일
-                const ableBlocks = blockMapper[blank.area] || [];
-                // 넓이가 동일한 모든 블록 삽입 가능한 모양인지 확인
-                for (const [bi, block] of ableBlocks.entries()) {
-                    let diff = -1; // 몇번 회전이 필요한지.
-                    // 모든 칸에 대하여 동일한지 비교
-                    for (const [chi, cellHistory] of blank.history.entries()) {
-                        const blockCellHistory = block.history[chi];
-                        if (cellHistory.length !== blockCellHistory.length) break; // 같지 않음. 삽입 불가
-                        for (const [di, d] of cellHistory.entries()) {
-                            const curDiff = Math.abs(d - blockCellHistory[di]);
-                            if (diff < 0) diff = curDiff;
-                            else if (diff !== curDiff) break; // 같지 않음. 삽입 불가
-                        }
-                    }
-                    // 같음 = 삽입 가능
-                    // TODO 삽입
-                    answer+=block.area;
-                    blockMapper[block.area].splice(bi, 1);
-                    break;
-                    //     // TODO 가능하나, 삽입하지 않음
-                }
+                blanks.push(blank);
             }
         })
     })
 
-    return answer;
+    // 3. 공백에 대해 맞는 블록들 확인
+    // let answer = 0;
+    let max = 0;
+    const checkBlankWithBlock = (total, idx, blockMapper) => {
+        if (idx >= blanks.length) {
+            max = Math.max(max, total);
+            return;
+        }
+
+        const blank = blanks[idx];
+        const ableBlocks = blockMapper[blank.area] || [];
+        // 넓이가 동일한 모든 블록 삽입 가능한 모양인지 확인
+        for (const [bi, block] of ableBlocks.entries()) {
+            let diff = -1; // 몇번 회전이 필요한지.
+            let flag = true;
+            // 모든 칸에 대하여 동일한지 비교
+            for (const [chi, cellHistory] of blank.history.entries()) {
+                const blockCellHistory = block.history[chi];
+                if (cellHistory.length !== blockCellHistory.length) {
+                    flag = false;
+                    break;
+                } // 같지 않음. 삽입 불가
+                for (const [di, d] of cellHistory.entries()) {
+                    const curDiff = Math.abs(d - blockCellHistory[di]);
+                    if (diff < 0) diff = curDiff;
+                    else if (diff !== curDiff) {
+                        flag = false;
+                        break;
+                    } // 같지 않음. 삽입 불가
+                }
+            }
+            // 같음 = 삽입 가능
+            // 1. 가능하나, 삽입하지 않음 -> 다음 가능한 블록 검사
+            // 2. 삽입 -> 다음 블럭 탐색한다.
+            // if (flag) {
+                const newBlockMapper = {...blockMapper};
+                newBlockMapper[block.area].splice(bi, 1);
+                checkBlankWithBlock(total + block.area, idx+1, newBlockMapper);
+            // }
+        }
+        checkBlankWithBlock(total, idx+1, blockMapper);
+    }
+    checkBlankWithBlock(0, 0, blockMapper);
+    return max;
 }
 
 const game_board = [[1,1,0,0,1,0],[0,0,1,0,1,0],[0,1,1,0,0,1],[1,1,0,1,1,1],[1,0,0,0,1,0],[0,1,1,1,0,0]];
