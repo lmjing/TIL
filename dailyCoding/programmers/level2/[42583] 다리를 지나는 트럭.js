80a3827ddfbc56334cc58ef388b37d12472315a4
mijeong lee <lmjing4905@gmail.com>
2022-03-08 00:50:40 +0900

    Update [42583] 다리를 지나는 트럭.js

--- dailyCoding/programmers/level2/[42583] 다리를 지나는 트럭.js
+++ dailyCoding/programmers/level2/[42583] 다리를 지나는 트럭.js
@@ -4,9 +4,14 @@ function solution(bridge_length, weight, truck_weights) {
     let sum = 0;
     
     while(truck_weights.length > 0) {
+        const isFinish = bridge.length > 0 && bridge[0].time + bridge_length === time;
+        if (isFinish) {
+            const first = bridge.shift();
+            time = bridge_length + first.time;
+            sum -= first.weight;
+        }
         const truck = truck_weights.shift();
         while(sum + truck > weight) {
-            // 커
             const first = bridge.shift();
             time = bridge_length + first.time - 1;
             sum -= first.weight;
@@ -20,4 +25,7 @@ function solution(bridge_length, weight, truck_weights) {
     
     return bridge_length + last.time;
 }
-// 미해결- TODO 테스트 케이스 더 확인 필요
+/*
+초과되지 않고, 시간이 다 지나서 다리를 다 지난 경우도 추가해야 함
+https://programmers.co.kr/questions/14215 
+*/
